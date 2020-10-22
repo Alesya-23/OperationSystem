2f3829081d07eb763b8afd2f3829636af1ad9d3a
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2020-10-22 13:28:48 +0300

    Commit 2

--- Cyclical planning/src/Thread.java
+++ Cyclical planning/src/Thread.java
@@ -2,7 +2,7 @@ public class Thread {
     private int treadID;
     private int time;
 
-    public Thread(int tID, int time) {
+    public Thread(int tID, int time ) {
         this.treadID = tID;
         this.time = time;
     }
@@ -13,10 +13,5 @@ public class Thread {
 
     public int getTreadID() {
         return treadID;
-    }
-
-
-    public void working() {
-        System.out.println( "поток " + treadID + " выполнился успешно за время " + time );
     }
 }
