d0f3d7cc0a32f2e5f4bd7f2484d63404b2a98265
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2020-11-30 14:14:54 +0300

    ChangeNodeFileSystem

--- Laba4/NodeFileSystem.java
+++ Laba4/NodeFileSystem.java
@@ -5,12 +5,13 @@ public class NodeFileSystem {
     public LinkedList<File> fileList;
     private int nodeIndex;
     private int fileIndex;
-    private File file;
 
     public NodeFileSystem(File file, int nodeIndex, int fileIndex) {
         nodeSize = 5;
         this.fileIndex = fileIndex;
-        this.file = file;
+        fileList = new LinkedList<File>();
+        if(file!=null)
+        fileList.add( file );
         this.nodeIndex = nodeIndex;
     }
 
@@ -28,22 +29,5 @@ public class NodeFileSystem {
 
     public void setNodeFileSystemIndex(int nodeIndex) {
         this.nodeIndex = nodeIndex;
-    }
-
-    public File getFile() {
-        return file;
-    }
-
-    public void setFile(File file) {
-        this.file = file;
-    }
-
-    public LinkedList<File> sortByNodeFileSystem(int nodeIndex) {
-        if (getNodeFileSystemIndex() == nodeIndex) {
-            for (int i = fileIndex; i < nodeSize; i++) {
-                fileList.add( getFile() );
-            }
-        }
-        return fileList;
     }
 }
